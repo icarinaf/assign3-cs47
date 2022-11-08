26f1894a8d21e5e40686850b422989833e6e3f6c
Carina Fung <icarinaf@stanford.edu>
2022-11-07 21:48:58 -0800

    modified assign3, assign4 submission

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
+import { StyleSheet } from "react-native";
 import "react-native-gesture-handler";
 import { WebView } from "react-native-webview";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import NewPage from "./app/components/newPage";
 import Main from "./app/components/main";
+import { Themes } from "./assets/Themes";
 
 export default function App() {
   const Stack = createStackNavigator();
@@ -16,8 +18,28 @@ export default function App() {
           component={Main}
           options={{ headerShown: false }}
         />
-        <Stack.Screen name="Song details" component={NewPage} />
-        <Stack.Screen name="Song preview" component={NewPage} />
+        <Stack.Screen
+          name="Song details"
+          component={NewPage}
+          options={{
+            headerStyle: {
+              backgroundColor: Themes.colors.background,
+            },
+            headerTitleStyle: { color: Themes.colors.white },
+            headerBackTitle: "Back",
+          }}
+        />
+        <Stack.Screen
+          name="Song preview"
+          component={NewPage}
+          options={{
+            headerStyle: {
+              backgroundColor: Themes.colors.background,
+            },
+            headerTitleStyle: { color: Themes.colors.white },
+            headerBackTitle: "Back",
+          }}
+        />
       </Stack.Navigator>
     );
   }
